77d3092472d82208c7192d700d0773d241bbb27d
Isahir <isahirzavala@hotmail.com>
2025-03-20 13:07:36 -0500

    Refactor the controller to handle the result response from the FileService

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -1,3 +1,4 @@
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Project1.DTOs;
@@ -11,8 +12,6 @@ namespace Project1.Controllers
     public class FilesController : ControllerBase
     {
         private readonly FileStorage _fileStorageService;
-        private const string containerName = "demoimages";
-
         public FilesController(FileStorage fileStorage)
         {
             _fileStorageService = fileStorage;
@@ -22,9 +21,7 @@ namespace Project1.Controllers
         public async Task<ActionResult<List<string>>> Get()
         {
             var blobServiceClient = _fileStorageService.GetBlobServiceClient();
-            var containerClient = _fileStorageService.GetContainerClient(blobServiceClient, containerName);
-
-            var listres = await _fileStorageService.ListBlobs(containerClient);
+            var listres = await _fileStorageService.ListBlobs(blobServiceClient);
 
             return Ok(listres);
 
@@ -34,35 +31,30 @@ namespace Project1.Controllers
         [Route("{blobName}")]
         public async Task<ActionResult> Get(string blobName)
         {
-            var blobServiceClient = _fileStorageService.GetBlobServiceClient();
-            var containerClient = _fileStorageService.GetContainerClient(blobServiceClient, containerName);
-
-            await _fileStorageService.DownloadBlob(blobName, containerClient);
-            Console.WriteLine("downloaded");
-            return Ok();
+            BlobServiceClient blobServiceClient = _fileStorageService.GetBlobServiceClient();
+            var res = await _fileStorageService.GetBlobURI(blobServiceClient);
+            if (!res.IsSuccess)
+            {
+                return BadRequest(res.ErrorMessage);
+            }
+            Uri blobUri = res.Value;
+            return Ok(blobUri);
         }
 
         [HttpDelete]
         [Route("{blobName}")]
         public async Task<ActionResult> Delete(string blobName)
         {
-
             var blobServiceClient = _fileStorageService.GetBlobServiceClient();
-            var containerClient = _fileStorageService.GetContainerClient(blobServiceClient, containerName);
-
-            await _fileStorageService.DeleteBlob(blobName, containerClient);
-            Console.WriteLine("Deleted");
+            await _fileStorageService.DeleteBlob(blobName, blobServiceClient);
             return Ok();
         }
 
         [HttpPost]
         public async Task<IActionResult> Post(UploadFileDTO uploadFileDTO)
         {
-
             var blobServiceClient = _fileStorageService.GetBlobServiceClient();
-            var containerClient = _fileStorageService.GetContainerClient(blobServiceClient, containerName);
-
-            var res = await _fileStorageService.UploadFileAsync(uploadFileDTO.File, containerClient);
+            var res = await _fileStorageService.UploadFileAsync(uploadFileDTO.File, blobServiceClient);
 
             if (!res.IsSuccess)
             {
