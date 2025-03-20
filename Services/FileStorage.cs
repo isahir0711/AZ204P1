b023133e43ed2395f56c08c11cf74c89b265e6f5
Isahir <isahirzavala@hotmail.com>
2025-03-20 13:01:30 -0500

    FileStorage Service ResultPattern and BlobServiceClient with Key
    Refactor the functions of FileStorage to return a Result object and replaced the creation of BlobServiceClient using ConnectionString now with SharedKey

--- Services/FileStorage.cs
+++ Services/FileStorage.cs
@@ -1,4 +1,8 @@
+using Azure.Storage;
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using Azure.Storage.Sas;
 using Project1.DTOs;
 using Project1.Enums;
 using Project1.ErrorHandling;
@@ -7,23 +11,80 @@ namespace Project1.Services
 {
     public class FileStorage
     {
+
+        private const string containerName = "demoimages";
+        private const string accountName = "iztestblob";
+        private const string accountEnvVarName = "STORAGEKEY";
         public BlobServiceClient GetBlobServiceClient()
         {
-            string blobConnectionString = Environment.GetEnvironmentVariable("BLOBCONN");
+            string accountKey = Environment.GetEnvironmentVariable(accountEnvVarName);
 
-            BlobServiceClient blobServiceClient = new(blobConnectionString);
+            if (string.IsNullOrEmpty(accountKey))
+            {
+                Console.WriteLine("Woops no account key");
+            }
+            StorageSharedKeyCredential storageSharedKeyCredential =
+                new(accountName, accountKey);
+            BlobServiceClient blobServiceClient = new BlobServiceClient(
+            new Uri($"https://{accountName}.blob.core.windows.net"),
+            storageSharedKeyCredential);
 
             return blobServiceClient;
         }
 
-        public BlobContainerClient GetContainerClient(BlobServiceClient blobServiceClient, string containerName)
+        public async Task<Result<Uri>> GetBlobURI(BlobServiceClient blobServiceClient)
         {
-            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
-            return containerClient;
+            BlobClient blobClient = blobServiceClient
+                .GetBlobContainerClient("demoimages")
+                .GetBlobClient("obscura_5_desktop.jpg");
+
+            //Todo validate if the blob exists
+
+            var res = await CreateServiceSASBlob(blobClient);
+
+            if (!res.IsSuccess)
+            {
+                return Result<Uri>.Failure(res.ErrorMessage);
+            }
+
+            Uri blobUri = res.Value;
+
+            return Result<Uri>.Success(blobUri);
         }
 
-        public async Task<Result<GetBlobDTO>> UploadFileAsync(IFormFile file, BlobContainerClient blobContainerClient)
+        public static async Task<Result<Uri>> CreateServiceSASBlob(BlobClient blobClient, string storedPolicyName = null)
         {
+            // Check if BlobContainerClient object has been authorized with Shared Key
+            if (!blobClient.CanGenerateSasUri)
+            {
+                return Result<Uri>.Failure("Client object is not authorized via Shared Key");
+            }
+            // Create a SAS token that's valid for one day
+            BlobSasBuilder sasBuilder = new BlobSasBuilder()
+            {
+                BlobContainerName = blobClient.GetParentBlobContainerClient().Name,
+                BlobName = blobClient.Name,
+                Resource = "b"
+            };
+
+            if (storedPolicyName == null)
+            {
+                sasBuilder.ExpiresOn = DateTimeOffset.UtcNow.AddMinutes(2);
+                sasBuilder.SetPermissions(BlobContainerSasPermissions.Read);
+            }
+            else
+            {
+                sasBuilder.Identifier = storedPolicyName;
+            }
+
+            Uri sasURI = blobClient.GenerateSasUri(sasBuilder);
+
+            return Result<Uri>.Success(sasURI);
+        }
+
+        public async Task<Result<GetBlobDTO>> UploadFileAsync(IFormFile file, BlobServiceClient blobServiceClient)
+        {
+            BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient("demoimages");
             long fileSizeBytes = file.Length;
             string fileName = file.FileName;
             string fileExtension = Path.GetExtension(fileName).Substring(1);
@@ -31,7 +92,7 @@ namespace Project1.Services
             {
                 return Result<GetBlobDTO>.Failure("Send an valid image file");
             }
-            if (fileSizeBytes > 5_000_000)
+            if (fileSizeBytes > 10_000_000)
             {
                 return Result<GetBlobDTO>.Failure("The image size must be lower than 5MB");
             }
@@ -40,7 +101,13 @@ namespace Project1.Services
 
             var stream = file.OpenReadStream();
 
-            await blobClient.UploadAsync(stream, true);
+            var Metadata = new Dictionary<string, string>
+                {
+                    { "ContentType", file.ContentType },
+                };
+
+            BlobHttpHeaders blobHttpHeaders = new BlobHttpHeaders { ContentType = file.ContentType };
+            await blobClient.UploadAsync(stream, new BlobUploadOptions { HttpHeaders = blobHttpHeaders });
 
             if (!await blobClient.ExistsAsync())
             {
@@ -52,8 +119,10 @@ namespace Project1.Services
 
         }
 
-        public async Task<List<string>> ListBlobs(BlobContainerClient blobContainerClient)
+        public async Task<List<string>> ListBlobs(BlobServiceClient blobServiceClient)
         {
+            BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient("demoimages");
+
             List<string> blobList = [];
 
             var resSegment = blobContainerClient.GetBlobsAsync();
@@ -66,26 +135,9 @@ namespace Project1.Services
             return blobList;
         }
 
-        public async Task DownloadBlob(string blobName, BlobContainerClient blobContainerClient)
+        public async Task DeleteBlob(string blobName, BlobServiceClient blobServiceClient)
         {
-            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
-            try
-            {
-                using var stream = await blobClient.OpenReadAsync();
-
-                FileStream blobStream = File.OpenWrite($"images/{blobClient.Name}");
-                await stream.CopyToAsync(blobStream);
-            }
-            catch (System.Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                throw;
-            }
-
-        }
-
-        public async Task DeleteBlob(string blobName, BlobContainerClient blobContainerClient)
-        {
+            BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient("demoimages");
             BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
 
             try
